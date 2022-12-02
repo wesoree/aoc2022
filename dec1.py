f5c53ecec89244c843213beadeb2054b202938a5
wesoree <75049633+wesoree@users.noreply.github.com>
2022-12-02 13:26:21 +0000

    update dec1.py

--- dec1.py
+++ dec1.py
@@ -5,16 +5,15 @@ with open('dec1input.txt', 'r', encoding='UTF-8') as inventory:
     for i in elflist:
         elves=map(int,i.split())
         a = sum(elves)
-        elfsum.append(a)    
-    def maxelves():
-        b = max(elfsum)
-        top3.append(b)
-        elfsum.remove(b)
-    maxelves()
-    maxelves()
-    maxelves()
-    c = sum(top3)
+        elfsum.append(a)
+    print(max(elfsum)) # this prints out the max value of total calories per elf, PART 1 ONLY
+    def maxelves(c):        # will find the top numbers depending on the num inside.
+        for num in range (0, c):    
+            b = max(elfsum)
+            top3.append(b)
+            elfsum.remove(b)
+    maxelves(3)
 
-    print(c)
+    print(sum(maxelves)) # part 2 only
 
     
