4c62756e2573c82545057092442f9aa4e93cb314
wesoree <wesleylo174@gmail.com>
2022-12-03 18:10:46 -0500

    repo release

--- day1.py
+++ day1.py
@@ -1,19 +1,19 @@
-with open('dec1input.txt', 'r', encoding='UTF-8') as inventory:
+with open('day1input.txt', 'r', encoding='UTF-8') as inventory:
     elflist = [line.rstrip() for line in inventory]
     elfsum = []
     top3 = []
     for i in elflist:
-        elves=map(int,i.split())
+        elves = map(int, i.split())
         a = sum(elves)
         elfsum.append(a)
-    print(max(elfsum)) # this prints out the max value of total calories per elf, PART 1 ONLY
-    def maxelves(c):        # will find the top numbers depending on the num inside.
-        for num in range (0, c):    
+    # part 1
+    print(max(elfsum))
+
+    def maxelves(c):
+        for num in range(0, c):
             b = max(elfsum)
             top3.append(b)
             elfsum.remove(b)
     maxelves(3)
-
-    print(sum(maxelves)) # part 2 only
-
-    
+    # part 2
+    print(sum(top3))
--- day3.py
+++ day3.py
@@ -1,7 +1,19 @@
-def partone():
-    pass
-def parttwo():
-    pass
+import string
+import itertools
 
-with open('day2input.txt') as data:
-    pass
+
+def prio(x):
+    return string.ascii_letters.index(x) + 1
+
+
+rucksacks = [line.strip() for line in open("day3input.txt").readlines()]
+# part 1
+common = itertools.chain(
+    *[list(set(r[:len(r)//2]).intersection(set(r[len(r)//2:]))) for r in rucksacks])
+print(sum(prio(c) for c in common))
+# part 2
+rs = [set(r) for r in rucksacks]
+badges = itertools.chain(
+    *[set.intersection(*r) for r in zip(rs[::3], rs[1::3], rs[2::3])]
+)
+print(sum(prio(b) for b in badges))
