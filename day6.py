2023b33bbb3d6e085ee136c2d9c2d00921d4a1b5
wesoree <wesleylo174@gmail.com>
2022-12-06 12:14:19 -0500

    add day 6

---
+++ day6.py
@@ -0,0 +1,20 @@
+from collections import deque
+
+with open('day6input.txt') as file:
+    line = file.read()
+
+def solve(length):
+    q = deque()
+    for i, c in enumerate(line):
+        q.append(c)
+        if len(q) > length:
+            q.popleft()
+        if len(set(q)) == length:
+            return i + 1
+# part 1
+
+print(solve(4)) 
+
+# part 2
+
+print(solve(14))
