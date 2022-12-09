c871d629a1a28ff8f414e50015603b641d9ac5d1
wesoree <wesleylo174@gmail.com>
2022-12-08 19:43:54 -0500

    format day 7

--- day7.py
+++ day7.py
@@ -2,23 +2,23 @@ from functools import cached_property
 from itertools import chain
 
 
-
-
 class File:
     def __init__(self, name, size, parent):
         self.name = name
         self.size = size
         self.parent = parent
-    
+
+
 class Directory:
-    def __init__(self, name, parent = None, children = None):
+    def __init__(self, name, parent=None, children=None):
         self.name = name
         self.parent = parent
         self.children = children or []
-    
+
     @cached_property
     def size(self):
         return sum(c.size for c in self.children)
+
 
 def build_file_tree(commands):
     root = Directory(name='root')
@@ -41,15 +41,18 @@ def build_file_tree(commands):
                 current_directory.children.append(item)
     return root
 
+
 def find_items(node, comparason):
-    dirs = [c for c in node.children if isinstance(c,Directory)]
+    dirs = [c for c in node.children if isinstance(c, Directory)]
     node = [node] if comparason(node.size) else []
     return node + list(chain.from_iterable(map(lambda c: find_items(c, comparason), dirs)))
+
 
 def sol1(input_string):
     root = build_file_tree(input_string.splitlines()[1:])
 
-    return print(sum(i.size for i in find_items(root, comparason=lambda n: n<=100000)))
+    return print(sum(i.size for i in find_items(root, comparason=lambda n: n <= 100000)))
+
 
 def sol2(input_string):
     root = build_file_tree(input_string.splitlines()[1:])
@@ -57,7 +60,9 @@ def sol2(input_string):
     free_space = 70000000 - root.size
     need_to_free = 30000000 - free_space
 
-    return print(min(find_items(root, lambda n: n>= need_to_free), key=lambda x: x.size).size)
+    return print(min(find_items(root, lambda n: n >= need_to_free), key=lambda x: x.size).size)
+
+
 if __name__ == '__main__':
     with open('day7input.txt') as f:
         f = f.read()
